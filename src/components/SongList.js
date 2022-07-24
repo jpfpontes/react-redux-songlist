ad5fd3e3c5e9b5ea21fd2c59e357847237dc8002
jpfpontes <joaopedromate@gmail.com>
2022-07-24 12:45:18 -0300

    feat: introduces the application

---
+++ src/components/SongList.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { selectSong } from "../actions";
+
+export class SongList extends Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primay"
+              onClick={() => {
+                this.props.selectSong(song);
+              }}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+const mapDispatchToProps = {
+  selectSong,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SongList);
